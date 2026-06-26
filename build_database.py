f001339fb73088d44b42a59dd0f41e2c82f5f3d2
Advaith Anand <advaithan25@iitk.ac.in>
2026-06-26 23:20:36 +0530

    Refactor build_database.py to use main function
    Refactor build_database.py to include a main function for better structure and readability.

--- build_database.py
+++ build_database.py
@@ -2,23 +2,32 @@
 build_database.py
 =================
 
-Builds the fingerprint database.
+Build the fingerprint database.
 
-Usage:
+Run:
 
 python build_database.py
 """
 
 from database import FingerprintDatabase
 
+
 SONG_FOLDER = "data/song_database"
 
 OUTPUT_DATABASE = "data/fingerprints.pkl"
 
-db = FingerprintDatabase()
 
-db.build(SONG_FOLDER)
+def main():
 
-db.statistics()
+    db = FingerprintDatabase()
 
-db.save(OUTPUT_DATABASE)
+    db.build(SONG_FOLDER)
+
+    db.statistics()
+
+    db.save(OUTPUT_DATABASE)
+
+
+if __name__ == "__main__":
+
+    main()
